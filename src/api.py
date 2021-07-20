bb4edea9bbfd616431f3f65de67aa6b1ced9a721
fshangala <fshangala@gmail.com>
2021-07-20 13:38:43 +0200

    api updated to export to json and open from a json file to allow for saving work

--- src/api.py
+++ src/api.py
@@ -1,8 +1,15 @@
+from json.decoder import JSONDecoder
+from json.encoder import JSONEncoder
+import os
+
 class Doc:
-    def __init__(self, name="index"):
+    def __init__(self, name="index", json=None):
         self.children = []
         self.childObjects = []
         self.name = name
+
+        if json != None:
+            self.new_from_json(json)
     
     def __str__(self):
         return "Document"
@@ -12,17 +19,47 @@ class Doc:
         element.osfed = str(index)
         self.childObjects.append(element)
         self.children.append(index)
+    
+    def new_from_json(self, json):
+        obj = JSONDecoder().decode(json)
+        self.name = obj["name"]
+        self.children = obj["children"]
+        self.childObjects = []
+        for child in obj["childObjects"]:
+            elem = El(json=JSONEncoder().encode(child))
+            self.childObjects.append(elem)
+    
+    def to_dict(self):
+        obj = dict()
+        obj["name"] = self.name
+        obj["children"] = self.children
+        obj["childObjects"] = list()
+        for childObject in self.childObjects:
+            obj["childObjects"].append(childObject.to_dict())
+        
+        return obj
 
 class Attribute:
-    def __init__(self, key, value=""):
+    def __init__(self, key="", value="", json=None):
         self.key = key
         self.value = value
+
+        if json != None:
+            obj = JSONDecoder().decode(json)
+            self.key = obj["key"]
+            self.value = obj["value"]
     
     def __str__(self):
         return "Attribute"
+    
+    def to_dict(self):
+        obj = dict()
+        obj["key"] = self.key
+        obj["value"] = self.value
+        return obj
 
 class El:
-    def __init__(self, name="p", type="closed", attributes=[]):
+    def __init__(self, name="p", type="closed", attributes=[], json=None):
         self.type = type
         self.children = []
         self.name = name
@@ -31,8 +68,26 @@ class El:
         self.osfed = ""
         self.innerHTML = ""
 
+        if json != None:
+            self.new_from_json(json)
+
     def __str__(self):
         return "Element"
+    
+    def to_dict(self):
+        obj = dict()
+        obj["osfed"] = self.osfed
+        obj["type"] = self.type
+        obj["name"] = self.name
+        obj["classes"] = self.classes
+        obj["attributes"] = list()
+        obj["innerHTML"] = self.innerHTML
+        obj["children"] = self.children
+
+        for attribute in self.attributes:
+            obj["attributes"].append(attribute.to_dict())
+        
+        return obj
         
     def addChild(self, element, document):
         index = len(document.childObjects)
@@ -42,6 +97,19 @@ class El:
 
     def add_class(self, class_name, document):
         self.classes.append(class_name)
+    
+    def new_from_json(self, json):
+        obj = JSONDecoder().decode(json)
+        self.osfed = obj["osfed"]
+        self.type = obj["type"]
+        self.name = obj["name"]
+        self.classes = obj["classes"]
+        self.attributes = []
+        self.innerHTML = obj["innerHTML"]
+        self.children = obj["children"]
+        for attribute in obj["attributes"]:
+            attr = Attribute(json=JSONEncoder().encode(attribute))
+            self.attributes.append(attr)
 
 class Text(El):
     def __init__(self, value=""):
@@ -70,6 +138,16 @@ def new_document(name="new"):
     h1.addChild(text, document)
 
     return document
+
+def open_from_file(filepath):
+    file = open(filepath, "r")
+    return Doc(json=file.read())
+
+def export_to_json(filepath, document):
+    file = open(filepath, "w")
+    obj = document.to_dict()
+    json = JSONEncoder().encode(obj)
+    file.write(json)
 
 def generateElement(elementIndex, document):
     element = document.childObjects[elementIndex]
@@ -110,4 +188,7 @@ def generateDocument(document):
 
     return html
 
-#print(generateDocument(new_document()))
+#document = open_from_file(os.path.dirname(__file__)+"/tmp/template.json")
+#document = new_document(name="sample")
+#export_to_json(os.path.dirname(__file__)+"/tmp/template.json", document)
+#print(generateDocument(document))
